6d921c9e2be887932b17341d72eeceeb0d72ad41
SkyLightQP <combbm@naver.com>
2020-06-20 00:17:43 +0900

    feat: apply color.css in index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './views/App';
+
+import './css/color.css';
 import './css/global.css';
 
 ReactDOM.render(
