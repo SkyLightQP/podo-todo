41a58ef737895f6f984c395bc2e0be0da819f2f8
SkyLightQP <combbm@naver.com>
2020-06-20 00:27:22 +0900

    feat: create SubTitle atomic for github link

---
+++ src/atomics/SubTitle/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const SubTitle = styled.p`
+    margin: 0;
+    text-align: center;
+    font-family: 'Noto Serif KR', serif;
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
+    color: #333333;
+    
+    &:hover {
+      text-shadow: rgba(0, 0, 0, 0.2) 3px 3px 10px;
+    }
+`;
+
+export default SubTitle;
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Title from '../atomics/Title';
+import Container from '../utils/Container';
+import TodoList from '../components/TodoList';
+import SubTitle from '../atomics/SubTitle';
+import styled from 'styled-components';
+
+const HeaderStyle = styled.header`
+    margin-top: 1rem;
+    margin-bottom: 1.5rem;
+`;
 
 const App: React.FC = () => {
-    return <></>;
+    return (
+        <Container>
+            <HeaderStyle>
+                <Title>Podo TODO</Title>
+                <a href="https://github.com/SkyLightQP/podo-todo/" target="_blank">
+                    <SubTitle>View on GitHub</SubTitle>
+                </a>
+            </HeaderStyle>
+            <TodoList />
+        </Container>
+    );
 };
 
 export default App;
