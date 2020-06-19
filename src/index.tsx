610064280cc559265075559a5798d51ab53680c8
SkyLightQP <combbm@naver.com>
2020-06-19 23:34:12 +0900

    feat: create global.css to apply default font and color
    - feat: add google font link

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './views/App';
+import './css/global.css';
 
 ReactDOM.render(
     <React.StrictMode>
