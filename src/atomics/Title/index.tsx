aa68d279b800db663129590be87a55e44e1327c0
SkyLightQP <combbm@naver.com>
2020-06-20 00:27:05 +0900

    feat: add Noto Serif KR font
    - fix: update title default font to Noto Serif KR

--- src/atomics/Title/index.tsx
+++ src/atomics/Title/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Title = styled.h1`
+    margin: 0;
     text-align: center;
+    font-family: 'Noto Serif KR', serif;
 `;
 
 export default Title;
