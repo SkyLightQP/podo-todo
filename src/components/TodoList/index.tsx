1562590ab0077427df8d382337161c9c2541d0be
SkyLightQP <combbm@naver.com>
2020-06-21 00:53:48 +0900

    feat: add `No Data` text when todo length is zero

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -2,20 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import TodoCard from '../TodoCard';
 import { entriesTodo } from '../../todo/TodoManager';
+import Title from '../../atomics/Title';
 
 const ListStyle = styled.div`
     display: flex;
     justify-content: center;
 `;
 
+const NoDataTextStyle = styled.p`
+    margin: 0;
+    text-align: center;
+    font-family: 'Noto Serif KR', serif;
+    font-size: 1.5rem;
+`;
+
 const TodoList: React.FC = () => {
+    const all = entriesTodo();
+    const todoMap = all.map((todo) => (
+        <TodoCard key={todo.date} data={{ content: todo.content, date: todo.date, done: todo.done }} />
+    ));
+
     return (
         <ListStyle>
-            <div>
-                {entriesTodo().map((todo) => (
-                    <TodoCard key={todo.date} data={{ content: todo.content, date: todo.date, done: todo.done }} />
-                ))}
-            </div>
+            <div>{all.length <= 0 ? <NoDataTextStyle>No Data</NoDataTextStyle> : todoMap}</div>
         </ListStyle>
     );
 };
