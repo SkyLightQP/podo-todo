bd633b5766cc2d5c193d00212df2ef356dee6b61
SkyLightQP <combbm@naver.com>
2020-06-21 00:49:42 +0900

    fix: ignore event when input value is empty

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../atomics/Title';
 import Container from '../utils/Container';
 import TodoList from '../components/TodoList';
 import SubTitle from '../atomics/SubTitle';
 import styled from 'styled-components';
+import TodoInput from '../components/TodoInput';
+import { addTodo } from '../todo/TodoManager';
 
 const HeaderStyle = styled.header`
     margin-top: 1rem;
@@ -11,6 +13,14 @@ const HeaderStyle = styled.header`
 `;
 
 const App: React.FC = () => {
+    const [input, setInput] = useState<string>('');
+
+    const onEnter = () => {
+        if (input === '') return;
+        addTodo(input);
+        setInput('');
+    };
+
     return (
         <Container>
             <HeaderStyle>
@@ -19,6 +29,16 @@ const App: React.FC = () => {
                     <SubTitle>View on GitHub</SubTitle>
                 </a>
             </HeaderStyle>
+
+            <TodoInput
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') onEnter();
+                }}
+                onClick={onEnter}
+            />
+
             <TodoList />
         </Container>
     );
