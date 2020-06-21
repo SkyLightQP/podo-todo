8adf900ef32f03e5cf142e331b572a1bdd17f7db
SkyLightQP <combbm@naver.com>
2020-06-21 18:53:39 +0900

    fix: remove TodoList component

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,19 +1,35 @@
 import React, { useState } from 'react';
 import Title from '../atomics/Title';
 import Container from '../utils/Container';
-import TodoList from '../components/TodoList';
 import SubTitle from '../atomics/SubTitle';
 import styled from 'styled-components';
 import TodoInput from '../components/TodoInput';
-import { addTodo } from '../todo/TodoManager';
+import { addTodo, entriesTodo } from '../todo/TodoManager';
+import TodoCard from '../components/TodoCard';
 
 const HeaderStyle = styled.header`
     margin-top: 1rem;
     margin-bottom: 1.5rem;
 `;
 
+const ListStyle = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const NoDataTextStyle = styled.p`
+    margin: 0;
+    text-align: center;
+    font-family: 'Noto Serif KR', serif;
+    font-size: 1.5rem;
+`;
+
 const App: React.FC = () => {
     const [input, setInput] = useState<string>('');
+    const all = entriesTodo();
+    const todoMap = all.map((todo) => (
+        <TodoCard key={todo.date} data={{ content: todo.content, date: todo.date, done: todo.done }} />
+    ));
 
     const onEnter = () => {
         if (input === '') return;
@@ -39,7 +55,9 @@ const App: React.FC = () => {
                 onClick={onEnter}
             />
 
-            <TodoList />
+            <ListStyle>
+                <div>{all.length <= 0 ? <NoDataTextStyle>No Data</NoDataTextStyle> : todoMap}</div>
+            </ListStyle>
         </Container>
     );
 };
